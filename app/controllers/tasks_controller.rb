6f04a027ccf037cf7490e581b1cdb932228f97d5
noriko tokuyama <akg.t.n05224@gmail.com>
2018-01-26 03:30:35 +0000

    login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,9 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks
   end
 
   def show
@@ -13,7 +14,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'task が正常に投稿されました'
@@ -52,5 +53,6 @@ class TasksController < ApplicationController
   
   def set_task
     @task = Task.find(params[:id])
+    redirect_to root_url if @task.user != current_user
   end
 end
